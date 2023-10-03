aa6f2fd641b2d2cba58242c422308c8612cd6935
Sourya Bhattacharya <souryabhattacharya21@gmail.com>
2023-10-03 19:57:20 +0530

    adding landing page full

--- snapdf/tailwind.config.ts
+++ snapdf/tailwind.config.ts
@@ -1,3 +1,4 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ["class"],
   content: [
@@ -71,5 +72,5 @@ module.exports = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
+  plugins: [require("tailwindcss-animate")],
 }
