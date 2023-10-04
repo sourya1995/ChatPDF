6d2cc943abb61ee07e5f570f1ad645af0ebc3fbe
Sourya Bhattacharya <souryabhattacharya21@gmail.com>
2023-10-04 12:21:41 +0530

    database connected

--- snapdf/src/app/dashboard/page.tsx
+++ snapdf/src/app/dashboard/page.tsx
@@ -1,11 +1,19 @@
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server"
 import { redirect } from "next/navigation"
+import { db } from "../db"
+import Dashboard from "@/components/Dashboard"
 
-const Page = () => {
+const Page = async () => {
     const {getUser} = getKindeServerSession()
     const user = getUser()
     if(!user || !user.id) redirect('/auth-callback?origin=dashboard')
-    return <div>{user.email}</div>
+    const dbUser = await db.user.findFirst({
+        where: {
+            id: user.id
+        }})
+
+    if(!dbUser) redirect('/auth-callback?origin=dashboard')
+    return <Dashboard />
 }
 
 export default Page
---
+++ snapdf/src/components/Dashboard.tsx
@@ -0,0 +1,8 @@
+const Dashboard = () => {
+    return (
+        <div>hello world</div>
+    )
+}
+
+
+export default Dashboard;
